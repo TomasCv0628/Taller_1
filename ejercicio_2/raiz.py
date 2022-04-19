d2e1ac4c43590e53a4e40150891ad51b2db89075
TomasCv0628 <tomas2224508@uis.edu.co>
2022-04-19 10:38:58 -0500

    Desarrollo: Ejercicios

---
+++ ejercicio_2/raiz.py
@@ -0,0 +1,20 @@
+"""Programa para hallar la raiz con exponente n"""
+
+print("-------------------------------------")
+print("-------- RAIZ CON INDICE n ----------")
+print("-------------------------------------")
+
+#input
+
+n = input("Indice de la raiz: ")
+n = int(n)
+X = input("Radicando de la raiz: ")
+X = int(X)
+
+#processing
+
+Z = X**(1/n)
+
+#output
+
+print("El valor de la raiz con indice " + str(n) + " del valor " + str(X) + " es igual a: " + str(Z))
